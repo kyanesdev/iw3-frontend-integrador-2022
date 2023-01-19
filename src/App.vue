97f56d204ad24823ddf5771420d538e4cc358173
kyanesdev <kyanesdev@gmail.com>
2023-01-19 14:21:01 -0300

    Se creó el login page

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,8 @@
 </script>
 
 <style>
- body{
- background:
-        /* top, transparent black, faked with gradient */ 
-        linear-gradient(
-          rgba(0, 0, 0, 0.7), 
-          rgba(0, 0, 0, 0.7)
-        ),
-        /* bottom, image */
-        url(https://ruta.ypf.com/images/analytics/analytics/img--containers.png);
-        
-        background-size: cover;
-    }
+
+body{
+  background-color: #F8C8C8;
+}
 </style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="contenedorLogin">
+    <div class="ingresoTitulo">
+      <p>Ingreso al sistema</p>
+    </div>
+
+    <div class="containerUsuario">
+      <div class="tituloBox">
+        <p>Usuario</p>
+      </div>
+
+      <input type="text"
+      placeholder="Ingrese su usuario">
+
+      </div>
+
+    <div class="containerContrasenia">
+      <div class="tituloBox">
+        <p>Contraseña</p>
+      </div>
+
+      <input type="password" placeholder="Ingrese su contraseña">
+    </div>
+
+    <button>Ingresar</button>
+
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.cdnfonts.com/css/kurale');
+@import url('https://fonts.cdnfonts.com/css/inter');
+.contenedorLogin{
+  position: absolute;
+  width: 412px;
+  height: 554px;
+  align-items: center;
+  left: 37%;
+  top: 15%;
+
+  background: #FFE1E1;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 31px;
+}
+
+.ingresoTitulo{
+  font-family: 'Kurale';
+  font-style: normal;
+  font-size: 33px;
+  text-align: center;
+  line-height: 6rem;
+}
+
+.containerUsuario{
+  text-align: center;
+}
+
+.tituloBox{
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 29px;
+  color: #000000;
+  
+}
+
+input{
+  color: #000000;
+  width: 90%;
+  height: 33px;
+  text-align: center;
+
+  background: #FFB3B3;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 53px;
+  border:0;outline:0;
+  
+}
+
+.containerContrasenia{
+  margin-top: 10%;
+  text-align: center;
+}
+
+::-webkit-input-placeholder{
+  color: #FFE1E1;
+  text-align: center;
+  -webkit-transition: all 500ms ease;
+  -moz-transition: all 500ms ease;
+  -ms-transition: all 500ms ease;
+  -o-transition: all 500ms ease;
+  transition: all 500ms ease;
+}
+
+
+:hover::-webkit-input-placeholder{
+  color:#000000;
+  -webkit-transition: all 500ms ease;
+  -moz-transition: all 500ms ease;
+  -ms-transition: all 500ms ease;
+  -o-transition: all 500ms ease;
+  transition: all 500ms ease;
+}
+
+button{
+  background: #FFB3B3;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 44px;
+
+  margin-top: 20%;
+  margin-left: 35%;
+  text-align: center;
+  width: 30%;
+  height: 8%;
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 29px;
+  color:#FFFFFF;
+  border:0;outline:0;
+  cursor:pointer;
+  -webkit-transition: all 500ms ease;
+  -moz-transition: all 500ms ease;
+  -ms-transition: all 500ms ease;
+  -o-transition: all 500ms ease;
+  transition: all 500ms ease;
+}
+
+button:hover{
+  background-color: #C42525;
+  -webkit-transition: all 500ms ease;
+  -moz-transition: all 500ms ease;
+  -ms-transition: all 500ms ease;
+  -o-transition: all 500ms ease;
+  transition: all 500ms ease;
+}
+
+
+</style>
