19cc48de34d4c269720fa6d6d535d77789fc2f79
kyanesdev <kyanesdev@gmail.com>
2023-01-20 14:34:27 -0300

    Solucion de tablas

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,22 @@
 body{
   background-color: #F8C8C8;
 }
+
+.p-datatable .p-datatable-header{
+  background-color: #ffe1e1 !important;
+  border-radius: 35px;
+  border: none;
+}
+
+td{
+  background-color: #ffe1e1 !important;
+  overflow: hidden;
+  border: none;
+}
+
+.p-datatable .p-paginator-bottom{
+  background-color: #ffe1e1 !important;
+  overflow: hidden;
+  border: none;
+}
 </style>
--- src/views/orden/OrdenPage.vue
+++ src/views/orden/OrdenPage.vue
@@ -1,51 +1,167 @@
 <template>
   <NavbarComp></NavbarComp>
   <div>
-    <Toolbar class="mb-4" id="toolBar">
-      <template #start>
-        <div class="btnToolbar">
-          <Button
-          label="Añadir"
-          icon="pi pi-plus"
-          class="p-button-success mr-2"
-          @click="openNew"
-          style="width: 50%;
-          margin-left: 5%;
-          margin-right: 5%;
-          text-align: center;
-          "
-        />
-        <Button
-          label="Eliminar"
-          icon="pi pi-trash"
-          class="p-button-danger"
-          style="width: 50%;
-          margin-left: 5%;
-          text-align: start;"
-          @click="confirmDeleteSelected"
-          :disabled="!selectedProducts || !selectedProducts.length"
-        />
-        </div>
-        
-      </template>
+    <div class="card">
+      <Toolbar class="mb-4" id="toolBar">
+        <template #start>
+          <div class="btnToolbar">
+            <Button
+              label="Añadir"
+              icon="pi pi-plus"
+              class="p-button-success mr-2"
+              @click="openNew"
+              style="
+                width: 50%;
+                margin-left: 5%;
+                margin-right: 5%;
+                text-align: center;
+              "
+            />
+            <Button
+              label="Eliminar"
+              icon="pi pi-trash"
+              class="p-button-danger"
+              style="width: 50%; margin-left: 5%; text-align: start"
+              @click="confirmDeleteSelected"
+              :disabled="!selectedProducts || !selectedProducts.length"
+            />
+          </div>
+        </template>
 
-      <template #end>
-        <FileUpload
-          mode="basic"
-          accept="image/*"
-          :maxFileSize="1000000"
-          label="Import"
-          chooseLabel="Import"
-          class="mr-2 inline-block"
-        />
-        <Button
-          label="Cambiar Estado"
-          icon="pi pi-bars"
-          class="p-button-help"
-          @click="exportCSV($event)"
-        />
-      </template>
-    </Toolbar>
+        <template #end>
+          <FileUpload
+            mode="basic"
+            accept="image/*"
+            :maxFileSize="1000000"
+            label="Import"
+            chooseLabel="Import"
+            class="mr-2 inline-block"
+          />
+          <Button
+            label="Cambiar Estado"
+            icon="pi pi-bars"
+            class="p-button-help"
+            @click="exportCSV($event)"
+          />
+        </template>
+      </Toolbar>
+
+      <div class="fondoTabla">
+        <DataTable
+          ref="dt"
+          :value="products"
+          v-model:selection="selectedProducts"
+          dataKey="id"
+          :paginator="true"
+          :rows="5"
+          :filters="filters"
+          style="background-color: #ffe1e1"
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          :rowsPerPageOptions="[5, 10, 25]"
+          currentPageReportTemplate="Mostrando {first} hasta {last} de {totalRecords} ordenes"
+          responsiveLayout="scroll"
+          class="dataTable"
+        >
+          <template #header>
+            <div
+              class="table-header flex flex-column md:flex-row md:justiify-content-between"
+              style="background-color: #ffe1e1"
+            >
+              <h1
+                class="mb-2 md:m-0 p-as-md-center"
+                style="text-align: center; font-size: 1.5rem"
+              >
+                Ordenes
+              </h1>
+            </div>
+          </template>
+
+          <Column
+            selectionMode="multiple"
+            style="width: 3rem; background-color: #ffe1e1"
+            :exportable="false"
+          ></Column>
+          <Column
+            field="N°"
+            header="N°"
+            :sortable="true"
+            style="min-width: 6rem; background-color: #ffe1e1"
+          ></Column>
+          <Column
+            field="Cliente"
+            header="Cliente"
+            :sortable="true"
+            style="min-width: 8rem; background-color: #ffe1e1"
+          ></Column>
+          <Column
+            field="Estado"
+            header="Estado"
+            :sortable="true"
+            style="min-width: 4rem; background-color: #ffe1e1"
+          >
+            <template #body="slotProps">
+              {{ formatCurrency(slotProps.data.price) }}
+            </template>
+          </Column>
+          <Column
+            field="Preset"
+            header="Preset"
+            :sortable="true"
+            style="min-width: 4rem; background-color: #ffe1e1"
+          ></Column>
+          <Column
+            field="CargaActual"
+            header="Carga actual"
+            :sortable="true"
+            style="min-width: 4rem; background-color: #ffe1e1"
+          >
+            <template #body="slotProps">
+              <Rating
+                :modelValue="slotProps.data.rating"
+                :readonly="true"
+                :cancel="false"
+              />
+            </template>
+          </Column>
+          <Column
+            field="Camion "
+            header="Camion"
+            :sortable="true"
+            style="min-width: 12rem; background-color: #ffe1e1"
+          >
+            <template #body="slotProps">
+              <span
+                :class="
+                  'product-badge status-' +
+                  (slotProps.data.inventoryStatus
+                    ? slotProps.data.inventoryStatus.toLowerCase()
+                    : '')
+                "
+                >{{ slotProps.data.inventoryStatus }}</span
+              >
+            </template>
+          </Column>
+          <Column
+            field="Acciones"
+            header="Acciones"
+            :sortable="true"
+            style="min-width: 6rem; background-color: #ffe1e1"
+          >
+            <template #body="slotProps">
+              <span
+                :class="
+                  'product-badge status-' +
+                  (slotProps.data.inventoryStatus
+                    ? slotProps.data.inventoryStatus.toLowerCase()
+                    : '')
+                "
+                >{{ slotProps.data.inventoryStatus }}</span
+              >
+            </template>
+          </Column>
+        </DataTable>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -76,17 +192,33 @@ export default {
 };
 </script>
 
-<style scoped>
-#toolBar{
+<style lang="scss" scoped>
+#toolBar {
   width: 60%;
   text-align: center;
   margin: auto;
   margin-top: 4rem;
   border-radius: 35px;
-  background-color: #FFE1E1;
+  background-color: #ffe1e1;
+  border: none;
 }
-.btnToolbar{
+
+.btnToolbar {
   display: flex;
   width: 17rem;
+}
+
+.dataTable{
+  width: 99%;
+  
+  border-radius: 36px;
+  margin: auto;
+}
+
+.fondoTabla{
+  width: 90%;
+  margin: auto;
+  border-radius: 35px;
+  background-color: #ffe1e1;
 }
 </style>
