304dd6ed6dec5e44a8d02315ba7064fd44dddb22
angelo59930 <manfrediangelosantiago10@gmail.com>
2023-02-02 19:34:35 -0300

    realizada la integracion del login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -9,23 +9,22 @@
         <p>Usuario</p>
       </div>
 
-      <input type="text"
-      placeholder="Ingrese su usuario">
+      <input id="user" type="text" placeholder="Ingrese su usuario">
 
-      </div>
+    </div>
 
     <div class="containerContrasenia">
       <div class="tituloBox">
         <p>Contraseña</p>
       </div>
 
-      <input type="password" placeholder="Ingrese su contraseña">
+      <input id="pass" type="password" placeholder="Ingrese su contraseña">
     </div>
 
     <button @click="ingresar()">Ingresar</button>
     <LoaderComp></LoaderComp>
   </div>
-  
+
 </template>
 
 <script>
@@ -33,19 +32,68 @@ import LoaderComp from "@/components/LoaderComp.vue"
 
 export default {
   name: "HomePage",
+  data() {
+    return {
+      user: "",
+      pass: "",
+    }
+  },
   components: {
     LoaderComp,
   },
   methods: {
-    ingresar(){
-      this.$router.push('/orden');
+    ingresar() {
+      this.user = document.getElementById("user").value;
+      this.pass = document.getElementById("pass").value;
+      try {
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+        let messageError = "No user registered with this details"
+
+        let urlencoded = new URLSearchParams();
+        urlencoded.append("username", this.user);
+        urlencoded.append("password", this.pass);
+        urlencoded.append("json", "true");
+
+        let requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: urlencoded,
+          redirect: 'follow'
+        };
+
+        fetch("http://localhost:8080/api/v1/login", requestOptions)
+          .then(response => response.text())
+          .then(result => {
+            if (result !== messageError){
+              sessionStorage.setItem("token", JSON.parse(result).authtoken);
+              this.goToHomePage();
+            }else{
+              this.addMessage();
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+
+      } catch (error) {
+        this.addMessage();
+      }
+    },
+    goToHomePage() {
+      this.$router.push('/orden')
+    },
+    addMessage() {
+      // TEMPORAL
+      alert("Usuario o contraseña incorrectos");
     }
   }
 };
 </script>
 
 <style scoped>
-.contenedorLogin{
+.contenedorLogin {
   width: 412px;
   height: 554px;
   margin: auto;
@@ -56,7 +104,7 @@ export default {
   border-radius: 31px;
 }
 
-.ingresoTitulo{
+.ingresoTitulo {
   font-family: 'Kurale';
   font-style: normal;
   font-size: 33px;
@@ -64,21 +112,21 @@ export default {
   line-height: 6rem;
 }
 
-.containerUsuario{
+.containerUsuario {
   text-align: center;
 }
 
-.tituloBox{
+.tituloBox {
   font-family: 'Inter';
   font-style: normal;
   font-weight: 400;
   font-size: 24px;
   line-height: 29px;
   color: #000000;
-  
+
 }
 
-input{
+input {
   color: #000000;
   width: 90%;
   height: 33px;
@@ -87,16 +135,17 @@ input{
   background: #FFB3B3;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 53px;
-  border:0;outline:0;
-  
+  border: 0;
+  outline: 0;
+
 }
 
-.containerContrasenia{
+.containerContrasenia {
   margin-top: 10%;
   text-align: center;
 }
 
-::-webkit-input-placeholder{
+::-webkit-input-placeholder {
   color: #FFE1E1;
   text-align: center;
   -webkit-transition: all 500ms ease;
@@ -107,8 +156,8 @@ input{
 }
 
 
-:hover::-webkit-input-placeholder{
-  color:#000000;
+:hover::-webkit-input-placeholder {
+  color: #000000;
   -webkit-transition: all 500ms ease;
   -moz-transition: all 500ms ease;
   -ms-transition: all 500ms ease;
@@ -116,7 +165,7 @@ input{
   transition: all 500ms ease;
 }
 
-button{
+button {
   background: #F9629F;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 44px;
@@ -131,9 +180,10 @@ button{
   font-weight: 400;
   font-size: 24px;
   line-height: 29px;
-  color:#FFFFFF;
-  border:0;outline:0;
-  cursor:pointer;
+  color: #FFFFFF;
+  border: 0;
+  outline: 0;
+  cursor: pointer;
   -webkit-transition: all 500ms ease;
   -moz-transition: all 500ms ease;
   -ms-transition: all 500ms ease;
@@ -141,7 +191,7 @@ button{
   transition: all 500ms ease;
 }
 
-button:hover{
+button:hover {
   background-color: #C42525;
   -webkit-transition: all 500ms ease;
   -moz-transition: all 500ms ease;
@@ -150,8 +200,8 @@ button:hover{
   transition: all 500ms ease;
 }
 
-@media (max-width: 800px) and (min-width: 400px){
-  input{
+@media (max-width: 800px) and (min-width: 400px) {
+  input {
     height: 60px;
     text-align: center;
     -webkit-transition: all 500ms ease;
@@ -160,20 +210,21 @@ button:hover{
     -o-transition: all 500ms ease;
     transition: all 500ms ease;
   }
-  button{
+
+  button {
     width: 50%;
     height: 10%;
     margin-left: 24.5%;
     margin-top: 15%;
   }
 
-  ::-webkit-input-placeholder{
+  ::-webkit-input-placeholder {
     font-size: 22px;
   }
 }
 
-@media (max-width: 1440px) and (min-width: 801px){
-  input{
+@media (max-width: 1440px) and (min-width: 801px) {
+  input {
     height: 50px;
     text-align: center;
     -webkit-transition: all 500ms ease;
@@ -182,20 +233,22 @@ button:hover{
     -o-transition: all 500ms ease;
     transition: all 500ms ease;
   }
-  button{
+
+  button {
     width: 35%;
     height: 10%;
     margin-left: 32.5%;
     margin-top: 15%;
   }
-  ::-webkit-input-placeholder{
+
+  ::-webkit-input-placeholder {
     font-size: 18px;
   }
 
 }
 
-@media (min-width: 1441px){
-  input{
+@media (min-width: 1441px) {
+  input {
     -webkit-transition: all 500ms ease;
     -moz-transition: all 500ms ease;
     -ms-transition: all 500ms ease;
@@ -203,5 +256,4 @@ button:hover{
     transition: all 500ms ease;
   }
 }
-
 </style>
