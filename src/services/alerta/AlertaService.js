9b0e3d302267068d89b948c24b050e57775ae45b
angelo59930 <manfrediangelosantiago10@gmail.com>
2023-02-02 19:50:39 -0300

    creado el servicio de alerta

---
+++ src/services/alerta/AlertaService.js
@@ -0,0 +1,47 @@
+export default class AlertaService {
+  constructor() {
+    this.url = "https://localhost:8080/api/v1/alerta";
+    this.token = sessionStorage.getItem("token");
+  }
+
+  async getAll() {
+    const response = await fetch(`${this.url}?authtoken=${this.token}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  }
+
+  async get(id) {
+    const response = await fetch(`${this.url}/${id}?authtoken=${this.token}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  }
+
+  async create(alerta) {
+    const response = await fetch(`${this.url}?authtoken=${this.token}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(alerta),
+    });
+    return await response.json();
+  }
+
+  async delete(id) {
+    const response = await fetch(`${this.url}/${id}?authtoken=${this.token}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  }
+}
