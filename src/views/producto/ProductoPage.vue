bbeb33d6719b02a82f7fd4b1cea4c99f401dc92d
angelo59930 <manfrediangelosantiago10@gmail.com>
2023-02-04 12:10:15 -0300

    creada la funcionalidad de guardar productos

--- src/views/producto/ProductoPage.vue
+++ src/views/producto/ProductoPage.vue
@@ -5,141 +5,89 @@
       <Toolbar class="mb-4" id="toolBar" style="margin-bottom:10px;">
         <template #start>
           <div class="btnToolbar">
-            <Button
-              label="Añadir"
-              icon="pi pi-plus"
-              class="p-button-success mr-2"
-              @click="openInsertDialog()"
-              style="
+            <Button label="Añadir" icon="pi pi-plus" class="p-button-success mr-2" @click="openInsertDialog()" style="
                 width: 50%;
                 margin-left: 5%;
                 margin-right: 5%;
                 text-align: center;
-              "
-            />
-            <Button
-              label="Eliminar"
-              icon="pi pi-trash"
-              class="p-button-danger"
-              style="width: 50%; margin-left: 5%; text-align: start"
-              @click="confirmDeleteSelected"
-              :disabled="!selectedProducts || !selectedProducts.length"
-            />
+              " />
+            <Button label="Eliminar" icon="pi pi-trash" class="p-button-danger"
+              style="width: 50%; margin-left: 5%; text-align: start" @click="confirmDeleteSelected"
+              :disabled="!selectedProducts || !selectedProducts.length" />
           </div>
         </template>
 
         <template #end>
-          <FileUpload
-            mode="basic"
-            accept="image/*"
-            :maxFileSize="1000000"
-            label="Import"
-            chooseLabel="Import"
-            class="mr-2 inline-block"
-          />
+          <FileUpload mode="basic" accept="image/*" :maxFileSize="1000000" label="Import" chooseLabel="Import"
+            class="mr-2 inline-block" />
         </template>
       </Toolbar>
 
       <div class="fondoTabla">
-        <DataTable
-          ref="dt"
-          :value="products"
-          v-model:selection="selectedProducts"
-          dataKey="id"
-          :paginator="true"
-          :rows="5"
-          :filters="filters"
-          style="background-color: #ffe1e1"
+        <DataTable ref="dt" :value="products" v-model:selection="selectedProducts" dataKey="id" :paginator="true"
+          :rows="5" :filters="filters" style="background-color: #ffe1e1"
           paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
           :rowsPerPageOptions="[5, 10, 25]"
-          currentPageReportTemplate="Mostrando {first} hasta {last} de {totalRecords} ordenes"
-          responsiveLayout="scroll"
-          class="dataTable"
-        >
+          currentPageReportTemplate="Mostrando {first} hasta {last} de {totalRecords} ordenes" responsiveLayout="scroll"
+          class="dataTable">
           <template #header>
-            <div
-              class="table-header flex flex-column md:flex-row md:justiify-content-between"
-              style="background-color: #ffe1e1"
-            >
-              <h1
-                class="mb-2 md:m-0 p-as-md-center"
-                style="text-align: center; font-size: 1.5rem; font-weight: normal;"
-              >
+            <div class="table-header flex flex-column md:flex-row md:justiify-content-between"
+              style="background-color: #ffe1e1">
+              <h1 class="mb-2 md:m-0 p-as-md-center"
+                style="text-align: center; font-size: 1.5rem; font-weight: normal;">
                 Productos
               </h1>
             </div>
           </template>
 
-          <Column
-            selectionMode="multiple"
-            style="width: 4rem; background-color: #ffe1e1"
-            :exportable="false"
-          ></Column>
-          <Column
-            field="nombre"
-            header="Nombre"
-            :sortable="true"
-            style="min-width: 6rem; background-color: #ffe1e1"
-          ></Column>
-          <Column
-            field="descripcion"
-            header="Descripción"
-            :sortable="true"
-            style="min-width: 8rem; background-color: #ffe1e1"
-          ></Column>
-          <Column
-            field="Acciones"
-            header="Acciones"
-            style="min-width: 6rem; background-color: #ffe1e1"
-          >
+          <Column selectionMode="multiple" style="width: 4rem; background-color: #ffe1e1" :exportable="false"></Column>
+          <Column field="nombre" header="Nombre" :sortable="true" style="min-width: 6rem; background-color: #ffe1e1">
+          </Column>
+          <Column field="descripcion" header="Descripción" :sortable="true"
+            style="min-width: 8rem; background-color: #ffe1e1"></Column>
+          <Column field="Acciones" header="Acciones" style="min-width: 6rem; background-color: #ffe1e1">
             <template #body="slotProps">
-              <span
-                :class="
-                  'product-badge status-' +
-                  (slotProps.data.inventoryStatus
-                    ? slotProps.data.inventoryStatus.toLowerCase()
-                    : '')
-                "
-                >{{ slotProps.data.inventoryStatus }}</span
-              >
+              <span :class="
+                'product-badge status-' +
+                (slotProps.data.inventoryStatus
+                  ? slotProps.data.inventoryStatus.toLowerCase()
+                  : '')
+              ">{{ slotProps.data.inventoryStatus }}</span>
             </template>
           </Column>
         </DataTable>
       </div>
     </div>
-    
+
     <Dialog v-model:visible="display">
-      
+
       <template #header>
         <h3>Añadir un producto</h3>
       </template>
 
       <div>
         <p>Nombre</p>
-        <input type="text" name="" placeholder="Ingrese el nombre"/>
+        <input type="text" name="" v-model="product.nombre" placeholder="Ingrese el nombre" />
 
         <p>Descripción</p>
-        <textarea name="" id="" cols="30" rows="10" placeholder="Describe aquí..."></textarea>
+        <textarea name="" id="" cols="30" rows="10" v-model="product.descripcion"
+          placeholder="Describe aquí..."></textarea>
       </div>
 
       <template #footer>
-        <Button label="Aceptar" icon="pi pi-check" />
-        <Button
-          label="Cancelar"
-          icon="pi pi-times"
-          class="p-button-text"
-          @click="closeDialog()"
-        />
+        <Button label="Aceptar" icon="pi pi-check" @click="save" />
+        <Button label="Cancelar" icon="pi pi-times" class="p-button-text" @click="closeDialog()" />
       </template>
+
     </Dialog>
-    
+
   </div>
 </template>
 
 <script>
 import NavbarComp from "@/components/NavbarComp.vue";
 import ProductoService from "@/services/producto/ProductoService";
-
+import Swal from 'sweetalert2'
 
 export default {
   components: {
@@ -180,6 +128,25 @@ export default {
     closeDialog() {
       this.display = false;
     },
+    save() {
+
+      console.log(this.product);
+      this.ProductoService.create(this.product).then((data) => {
+        console.log(data);
+      });
+      Swal.fire({
+        icon: 'success',
+        title: `Guardado Correctamente`,
+        showConfirmButton: false,
+        timer: 1500,
+      })
+
+      setTimeout(() => {
+        this.display = false;
+        this.$router.go();
+      }, 1500);
+
+    },
   },
 };
 </script>
@@ -213,7 +180,7 @@ export default {
   background-color: #ffe1e1;
 }
 
-input{
+input {
   color: #000000;
   width: 90%;
   height: 33px;
@@ -222,11 +189,12 @@ input{
   background: #ffe1e1;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 53px;
-  border:0;outline:0;
-  
+  border: 0;
+  outline: 0;
+
 }
 
-::-webkit-input-placeholder{
+::-webkit-input-placeholder {
   color: #FFB3B3;
   text-align: center;
   -webkit-transition: all 500ms ease;
@@ -237,8 +205,8 @@ input{
 }
 
 
-:hover::-webkit-input-placeholder{
-  color:#000000;
+:hover::-webkit-input-placeholder {
+  color: #000000;
   -webkit-transition: all 500ms ease;
   -moz-transition: all 500ms ease;
   -ms-transition: all 500ms ease;
@@ -246,7 +214,7 @@ input{
   transition: all 500ms ease;
 }
 
-textarea{
+textarea {
   resize: none;
   color: #000000;
   width: 100%;
@@ -256,11 +224,12 @@ textarea{
   background: #ffe1e1;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
-  border:0;outline:0;
+  border: 0;
+  outline: 0;
 }
 
-@media (max-width: 800px) and (min-width: 400px){
-  input{
+@media (max-width: 800px) and (min-width: 400px) {
+  input {
     height: 60px;
     text-align: center;
     -webkit-transition: all 500ms ease;
@@ -269,13 +238,15 @@ textarea{
     -o-transition: all 500ms ease;
     transition: all 500ms ease;
   }
-  button{
+
+  button {
     width: 50%;
     height: 10%;
     margin-left: 24.5%;
     margin-top: 15%;
   }
-  textarea{
+
+  textarea {
     height: 260px;
     -webkit-transition: all 500ms ease;
     -moz-transition: all 500ms ease;
@@ -283,13 +254,14 @@ textarea{
     -o-transition: all 500ms ease;
     transition: all 500ms ease;
   }
-  ::-webkit-input-placeholder{
+
+  ::-webkit-input-placeholder {
     font-size: 22px;
   }
 }
 
-@media (max-width: 1440px) and (min-width: 801px){
-  input{
+@media (max-width: 1440px) and (min-width: 801px) {
+  input {
     height: 50px;
     text-align: center;
     -webkit-transition: all 500ms ease;
@@ -298,13 +270,15 @@ textarea{
     -o-transition: all 500ms ease;
     transition: all 500ms ease;
   }
-  button{
+
+  button {
     width: 35%;
     height: 10%;
     margin-left: 32.5%;
     margin-top: 15%;
   }
-  textarea{
+
+  textarea {
     height: 210px;
     -webkit-transition: all 500ms ease;
     -moz-transition: all 500ms ease;
@@ -312,20 +286,22 @@ textarea{
     -o-transition: all 500ms ease;
     transition: all 500ms ease;
   }
-  ::-webkit-input-placeholder{
+
+  ::-webkit-input-placeholder {
     font-size: 18px;
   }
 }
 
-@media (min-width: 1441px){
-  input{
+@media (min-width: 1441px) {
+  input {
     -webkit-transition: all 500ms ease;
     -moz-transition: all 500ms ease;
     -ms-transition: all 500ms ease;
     -o-transition: all 500ms ease;
     transition: all 500ms ease;
   }
-  textarea{
+
+  textarea {
     height: 180px;
     -webkit-transition: all 500ms ease;
     -moz-transition: all 500ms ease;
@@ -334,5 +310,4 @@ textarea{
     transition: all 500ms ease;
   }
 }
-
 </style>
